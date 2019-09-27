8c2b0ef10ca235d7036a1350f40a8a205dccd85d
Justin Yau <justiceman8998@gmail.com>
2019-09-26 22:04:03 -0400

    Implemented Player Model Class and the basis of PostSignInRoute (Untested)

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,4 +1,32 @@
 package com.webcheckers.model;
 
+/**
+ * This is the model for a single player entity.
+ *
+ * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
+ * @author <a href='mailto:jrv@se.rit.edu'>Jim Vallino</a>
+ */
 public class Player {
+
+  // Field where the player username will be stored
+  private String username;
+
+  /**
+   * Create a new player entity with a reserved username
+   *
+   * @param username
+   *          Username for the player entity
+   */
+  public Player(String username) {
+    this.username = username;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public synchronized String toString() {
+    return this.username;
+  }
+
 }
--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -1,4 +1,48 @@
 package com.webcheckers.ui;
 
-public class PostSignInRoute {
+import spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+/**
+ * The UI controller to POST the SIGN-IN Page
+ *
+ * @author Justin Yau
+ */
+public class PostSignInRoute implements Route {
+
+  private static final Logger LOG = Logger.getLogger(PostSignInRoute.class.getName());
+
+  private final TemplateEngine templateEngine;
+
+  public PostSignInRoute(final TemplateEngine templateEngine) {
+    this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+    //
+    LOG.config("PostSignInRoute is initialized.");
+  }
+
+  /**
+   * Updates the WebCheckers Sign-In page.
+   *
+   * @param request
+   *   the HTTP request
+   * @param response
+   *   the HTTP response
+   *
+   * @return
+   *   the rendered HTML for the Sign-In page
+   */
+  @Override
+  public Object handle(Request request, Response response) {
+    LOG.finer("PostSignInRoute is invoked.");
+    //
+    Map<String, Object> vm = new HashMap<>();
+
+    // render the View
+    return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
+  }
+
 }
