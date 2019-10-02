84e79688d02e8fefdfaf433f1fd502278b733f25
Brian Mirabito <bjm9265@rit.edu>
2019-10-01 22:25:52 -0400

    initial commit for gamecenter work

--- src/main/java/com/webcheckers/appl/GameCenter.java
+++ src/main/java/com/webcheckers/appl/GameCenter.java
@@ -1,4 +1,17 @@
 package com.webcheckers.appl;
 
+import com.webcheckers.model.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameCenter {
+
+    private Map<String, Player> players;
+    private PlayerLobby playerLobby;
+
+    public GameCenter(PlayerLobby playerLobby) {
+        players = new HashMap<>() ;
+        this.playerLobby = playerLobby;
+    }
 }
