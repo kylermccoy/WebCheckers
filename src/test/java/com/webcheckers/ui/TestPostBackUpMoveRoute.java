912f18a576f549c853ba635ed0637ecade661fc0
Brian Mirabito <bjm9265@rit.edu>
2019-11-05 17:56:34 -0500

    backup move test complete
    Signed-off-by: Brian Mirabito <bjm9265@rit.edu>

--- src/test/java/com/webcheckers/ui/TestPostBackUpMoveRoute.java
+++ src/test/java/com/webcheckers/ui/TestPostBackUpMoveRoute.java
@@ -3,9 +3,7 @@ package com.webcheckers.ui;
 import com.google.gson.Gson;
 import com.webcheckers.appl.GameCenter;
 import com.webcheckers.appl.PlayerLobby;
-import com.webcheckers.model.Player;
-import com.webcheckers.model.Turn;
-import com.webcheckers.util.Message;
+import com.webcheckers.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import spark.Request;
@@ -25,29 +23,41 @@ public class TestPostBackUpMoveRoute {
     // Mocked Objects
     private Session session;
     private Request request;
-    private TemplateEngine engine;
-    private PlayerLobby playerLobby;
     private GameCenter center;
     private Gson gson;
+    private CheckersGame game;
 
     @BeforeEach
     public void initializeBackUpMove(){
         request = mock(Request.class);
         session = mock(Session.class);
         when(request.session()).thenReturn(session);
-        engine = mock(TemplateEngine.class);
         gson = new Gson();
         center = new GameCenter();
-        playerLobby = new PlayerLobby(center);
+        game = mock(CheckersGame.class);
 
         backUpMove = new PostBackUpMoveRoute(gson, center);
+    }
+
+    @Test
+    public void test_backUpSuccessful(){
+        Player player = new Player("Dan");
+        Player player2 = new Player("Bob");
+        game = center.startGame(player, player2);
+        when(session.attribute(GetHomeRoute.CURRENT_USER_KEY)).thenReturn(player);
+        Turn turn = center.getPlayerTurn(player);
+        Move move = new Move(new Position(5, 2), new Position(4, 3));
+        turn.validateMove(move);
+        final Response response = mock(Response.class);
+        String actualValue = backUpMove.handle(request, response).toString();
+        assertEquals("{\"text\":\"BackUp successful.\",\"type\":\"INFO\"}", actualValue);
     }
 
     @Test
     public void test_backUpUnSuccessful(){
         Player player = new Player("Dan");
         Player player2 = new Player("Bob");
-        center.startGame(player, player2);
+        game = center.startGame(player, player2);
         when(session.attribute(GetHomeRoute.CURRENT_USER_KEY)).thenReturn(player);
         final Response response = mock(Response.class);
         String actualValue = backUpMove.handle(request, response).toString();
